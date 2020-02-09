567255f29d33d81cc92b86c0e663b9e473b1dc35
rwalton00 <rwalton00@gmail.com>
2020-02-09 14:18:45 +0000

    make io port arguments mandatory (#13)
    * make io port arguments mandatory
    * improve type annotation

--- src/midiroute/cli.py
+++ src/midiroute/cli.py
@@ -2,7 +2,7 @@
 
 import asyncio
 
-from typing import Any
+from typing import List
 
 import click
 import tabulate
@@ -17,10 +17,17 @@ def cli() -> None:
 
 
 @cli.command()
-@click.option("--input-port", "-i", help="Select the input port.", metavar="PORT_NAME")
+@click.option(
+    "--input-port",
+    "-i",
+    help="Select the input port.",
+    required=True,
+    metavar="PORT_NAME",
+)
 @click.option(
     "--output-ports",
     "-o",
+    required=True,
     help="Select one or more output ports in a comma separated list.",
     metavar="PORT_NAME",
 )
@@ -30,10 +37,10 @@ def cli() -> None:
     is_flag=True,
     help="Enable monitoring of MIDI activity on the selected ports.",
 )
-def run(input_port: str, output_ports: Any, monitor: bool) -> None:
+def run(input_port: str, output_ports: str, monitor: bool) -> None:
     """Run the MIDI router."""
-    output_ports = output_ports.split(",")
-    asyncio.run(midiutils.stream(input_port, output_ports, monitor))
+    output_ports_list: List[str] = output_ports.split(",")
+    asyncio.run(midiutils.stream(input_port, output_ports_list, monitor))
 
 
 @cli.command()
